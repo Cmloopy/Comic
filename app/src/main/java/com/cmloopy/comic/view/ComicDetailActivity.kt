09296dc7ecae6116dc04c69ab0b8b44bcce736e9
Dang Minh Anh <dangminhanh4656@gmail.com>
2025-03-06 15:00:45 +0700

    "06/03/2025 - "15:00"

--- app/src/main/java/com/cmloopy/comic/view/ComicDetailActivity.kt
+++ app/src/main/java/com/cmloopy/comic/view/ComicDetailActivity.kt
@@ -1,12 +1,12 @@
 package com.cmloopy.comic.view
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cmloopy.comic.R
 import com.cmloopy.comic.adapters.CategoryAdapter
 import com.cmloopy.comic.adapters.ListChapAdapter
@@ -15,13 +15,16 @@ import com.cmloopy.comic.data.api.CategoryApi
 import com.cmloopy.comic.data.api.ChapterApi
 import com.cmloopy.comic.data.api.ComicApi
 import com.cmloopy.comic.databinding.ActivityComicDetailBinding
-import com.cmloopy.comic.models.Category
+import com.cmloopy.comic.models.Chapter
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textview.MaterialTextView
 import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.create
 
 class ComicDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityComicDetailBinding
@@ -30,8 +33,10 @@ class ComicDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityComicDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        //Nhận id comic từ intent
         val idComic = intent.getIntExtra("idComic",-1)
+
+        //Setup Data cho Activity từ API
         lifecycleScope.launch {
             try {
                 val apiComicService = RetrofitClient.instance.create(ComicApi::class.java)
@@ -66,21 +71,20 @@ class ComicDetailActivity : AppCompatActivity() {
 
                 binding.rclCategory.layoutManager = layoutManager
                 binding.rclCategory.adapter = CategoryAdapter(category)
+
+                //Click xem all list
+                binding.btnListChapAll.setOnClickListener {
+                    setUpBottomSheetListChap(idComic, comic[0].sc, chapter)
+                }
             }
             catch (e: Exception){
-
+                println(e)
             }
         }
-        //Click
-        btnComicDetailsOnClick()
-    }
 
-    private fun btnComicDetailsOnClick() {
+        //Click
         binding.btnBackPrevious.setOnClickListener {
             onBackPressed()
-        }
-        binding.btnListChapAll.setOnClickListener {
-
         }
         binding.btnDtdau.setOnClickListener {
 
@@ -91,5 +95,48 @@ class ComicDetailActivity : AppCompatActivity() {
         binding.btnFollow.setOnClickListener {
 
         }
+    }
+
+    private fun setUpBottomSheetListChap(idComic: Int, slChap : Int, chapter: ArrayList<Chapter>) {
+        val dialog = BottomSheetDialog(this)
+        val view = layoutInflater.inflate(R.layout.bottom_sheet_list_chap, null)
+        //Setup recycleView
+        val listAll = view.findViewById<RecyclerView>(R.id.rcl_listFullChap)
+        listAll.layoutManager = LinearLayoutManager(this)
+        listAll.adapter = ListChapAdapter(chapter)
+        val txtSL = view.findViewById<MaterialTextView>(R.id.txt_capnhat)
+        txtSL.text = "Cập nhật đến chap ${slChap}"
+        //Setup btn
+        val btnOld = view.findViewById<MaterialButton>(R.id.btn_oldest)
+        val btnNew = view.findViewById<MaterialButton>(R.id.btn_newest)
+        val chapterNw = chapter
+        btnOld.setOnClickListener {
+            val chapterRv = ArrayList(chapter.reversed())
+            listAll.adapter = ListChapAdapter(chapterRv)
+            listAll.run { adapter?.notifyDataSetChanged() }
+            btnOld.setTextColor(resources.getColor(R.color.btn))
+            btnNew.setTextColor(resources.getColor(R.color.text2))
+        }
+        btnNew.setOnClickListener {
+            listAll.adapter = ListChapAdapter(chapterNw)
+            listAll.run { adapter?.notifyDataSetChanged() }
+            btnNew.setTextColor(resources.getColor(R.color.btn))
+            btnOld.setTextColor(resources.getColor(R.color.text2))
+        }
+
+        view.post {
+            val bottomSheet = dialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+            bottomSheet?.let {
+                val behavior = BottomSheetBehavior.from(it)
+                behavior.peekHeight = 600 // Đặt chiều cao cố định
+                behavior.state = BottomSheetBehavior.STATE_EXPANDED // Giữ trạng thái mở rộng
+                behavior.isDraggable = false // Không cho phép kéo lên/xuống
+            }
+        }
+
+        //Tắt sheet khi bấm ra ngoài
+        dialog.setCancelable(true)
+        dialog.setContentView(view)
+        dialog.show()
     }
 }
