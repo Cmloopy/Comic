3eabafbae465cd5ec6ca9ad63d6f817cb741bb4d
Dang Minh Anh <dangminhanh4656@gmail.com>
2025-03-14 14:29:37 +0700

    14/03/2025 - 14:29

--- app/src/main/java/com/cmloopy/comic/view/LoginActivity.kt
+++ app/src/main/java/com/cmloopy/comic/view/LoginActivity.kt
@@ -1,7 +1,6 @@
 package com.cmloopy.comic.view
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.cmloopy.comic.databinding.ActivityLoginBinding
 
@@ -12,25 +11,9 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLoginGoogle.setOnClickListener {
-            GoogleLogin()
-        }
-        binding.btnLoginFacebook.setOnClickListener {
-            FaceBookLogin()
-        }
-        binding.btnLoginLine.setOnClickListener {
-            Toast.makeText(this,"Chức năng đang cập nhật!", Toast.LENGTH_SHORT).show()
-        }
-        binding.btnLoginWhatsapp.setOnClickListener {
-            Toast.makeText(this,"Chức năng đang cập nhật!", Toast.LENGTH_SHORT).show()
-        }
-    }
-    //Cấu hình Google Login
-    private fun GoogleLogin(){
+        binding.materialButtonLG.setOnClickListener {
 
+        }
     }
-    //Cấu hình Facebook Login
-    private fun FaceBookLogin(){
 
-    }
 }
